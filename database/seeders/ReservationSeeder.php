27bb144cfc334f3eb5e40311a8dea3037f79b78b
Carol <climunoz@poligran.edu.co>
2024-05-27 14:07:15 -0500

    Se agrega Seeder

---
+++ database/seeders/ReservationSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Reservation;
+use App\Models\Table;
+use Carbon\Carbon;
+
+
+class ReservationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         // Crear o asegurar la existencia de dos mesas
+         $table1 = Table::firstOrCreate(['name' => 'Mesa 1']);
+         $table2 = Table::firstOrCreate(['name' => 'Mesa 2']);
+ 
+         // Crear una reservación
+         Reservation::create([
+             'table_id' => $table1->id,
+             'customer_name' => 'John Doe',
+             'customer_email' => 'john.doe@example.com',
+             'customer_phone' => '1234567890',
+             'date' => Carbon::today()->toDateString(),
+             'hour' => Carbon::now()->format('H:i'),
+         ]);
+    }
+}
